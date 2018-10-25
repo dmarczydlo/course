b6fa442daa86da95f67af42ab55b82b3f4a5a7fa
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-10-25 17:33:34 +0200

    token

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,21 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
-
+const kwt = require('jsonwebtoken');
 
 const server = createServer();
 
 server.express.use(cookieParser());
-//TODO user express middleware to populate current user
+
+
+server.express.use((req, res, next) => {
+    const { token } = req.cookies;
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        req.userId = userId;
+    }
+    next();
+})
 
 
 server.start({
